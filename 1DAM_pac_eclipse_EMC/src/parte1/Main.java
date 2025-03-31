d322936adcd26717364d771bede2543441a70f74
Quique_m <enriqueminguetcobos1@gmail.com>
2025-03-31 11:36:44 +0200

    Update Main.java

--- 1DAM_pac_eclipse_EMC/src/parte1/Main.java
+++ 1DAM_pac_eclipse_EMC/src/parte1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola! soy un mensaje de texto");
-
+	//Comentario realizado desde github: Falta comentarios y scanneres
 	}
 
 }
